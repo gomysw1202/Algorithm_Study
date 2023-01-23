5990b8b7906ff71ece8a1acd21e68b56ac7ca0c7
임선우(Lim Seon Woo) <gomysw1202@gmail.com>
2023-01-23 19:01:15 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String input_id = sc.nextLine();
+		System.out.println(input_id + "??!");
+		
+		sc.close();
+	}
+}
