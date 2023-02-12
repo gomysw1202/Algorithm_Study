263447063c0e9fdc188fedb5217f54c7a0fa4072
임선우(Lim Seon Woo) <gomysw1202@gmail.com>
2023-02-12 13:43:49 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 204 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-
-        if(A<B){
-            System.out.print("<");
-        }
-        else if(A>B) {
-            System.out.print(">");
-        }
-        else {
-            System.out.print("==");
-        }
-        sc.close();
-    }
+public class Main {//25083
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		String y = "";
+		if(A==B) {
+			y="==";
+		}
+		else if(A<B) {
+			y="<";
+		}
+		else {
+			y=">";
+		}
+		
+		System.out.println(y);
+		
+		sc.close();
+	}
 }
