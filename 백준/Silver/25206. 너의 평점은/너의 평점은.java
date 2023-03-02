7e8c67a16d28db4f44f31dc27fe8aa0c508a8b04
임선우(Lim Seon Woo) <gomysw1202@gmail.com>
2023-03-02 14:24:14 +0900

    [Silver V] Title: 너의 평점은, Time: 128 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+       String[] str = new String[20];
+       double totalSum = 0;
+       double scoreSum = 0;
+       String[] gradeList = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F", "P"};
+       double[] gradeScore = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0, 0.0};
+
+       StringTokenizer st;
+       for(int i=0; i<20; i++) {
+           str[i] = br.readLine();
+           st = new StringTokenizer(str[i]," ");
+           st.nextToken();
+           double score = Double.parseDouble(st.nextToken());
+           String grade = st.nextToken();
+
+           for(int j=0; j<10; j++) {
+               if(grade.equals(gradeList[j])){
+                   totalSum += score * gradeScore[j];
+                   if(j != 9) {
+                       scoreSum += score;
+                   }
+               }
+           }
+       }
+       System.out.println(totalSum / scoreSum);
+       br.close();
+    }
+}
