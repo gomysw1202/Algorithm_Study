4a42e55a55b550bac2890dab37caebcf343fcf9f
임선우(Lim Seon Woo) <gomysw1202@gmail.com>
2023-04-03 19:22:01 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 204 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int sum = a+b+c;
+
+        if (sum == 180) {
+            if(a==b && b==c) {
+                System.out.println("Equilateral");
+            }
+            else if((a==b&&b!=c) || (a!=b&& b==c) ||(a==c && b!=c)) {
+                System.out.println("Isosceles");
+            }
+            else if(a!=b && a!=c && b!=c) {
+                System.out.println("Scalene");
+            }
+        }else {
+            System.out.println("Error");
+        }
+
+
+    }
+}
