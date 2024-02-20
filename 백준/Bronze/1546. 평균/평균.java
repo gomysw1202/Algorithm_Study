62bca929c484a2e842b11ae205b36916f09ce45f
임선우(Lim Seon Woo) <gomysw1202@gmail.com>
2024-02-20 22:26:34 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,35 +1,34 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[]args) throws IOException {
+class Main {
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());// 과목수 입력
-        StringTokenizer st = new StringTokenizer(br.readLine()); // 점수 입력
-        double[] arr = new double[n]; // 점수를 담는 배열 생성
+        int n = Integer.parseInt(br.readLine());
 
-        double max = 0; // 최댓값
-        int num = 0; // 최대값의 인덱스
-        double sum = 0; // 과목 총 합
-        for(int i=0; i<arr.length; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-            if(max<arr[i]) {
-                max = arr[i];
-                num = i;// 최댓값 인덱스 담기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double[] arr = new double[n];
+        int i=0;
+        double max = 0;
+        double sum=0;
+
+
+        for(int j=0; j<n; j++){
+            arr[j] = Integer.parseInt(st.nextToken());
+            if(max<arr[j]){
+                max = arr[j];
+                i = j;
             }
         }
 
-        for (int i=0; i<arr.length; i++) {
-            arr[i] = (arr[i] / max) * 100;
-            sum += arr[i];
+        for(int j=0; j<n; j++){
+            arr[j] = arr[j]/max * 100;
+            sum += arr[j];
         }
 
         System.out.println(sum/n);
-
     }
 }
