fe1e1149da32bbbd8d705c65a19ed9030798848f
임선우(Lim Seon Woo) <gomysw1202@gmail.com>
2024-02-26 18:57:37 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,31 +1,26 @@
 import java.io.*;
-
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String n = bf.readLine();
-        String[] arr = new String[26];
-        int m = 97;
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = String.valueOf((char) m);
-            m += 1;
-        }
-        int[] result = new int[27];
-        for (int r = 0; r < result.length; r++) {
-            result[r] = -1;
-        }
-        for (int i = 1; i < 27; i++) {
-            for (int j = 0; j < n.length(); j++) {
-                if (String.valueOf(n.charAt(j)).equals(arr[i-1])){
-                    result[i] = j;
-                    break;
-                }
-            }
-            bw.write(result[i] + " ");
-        }
-        bw.flush();
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] arr = br.readLine().toCharArray();
+        int[] arr2 = new int[26];
+
+        for(int i=0; i<arr2.length; i++){
+            arr2[i] = -1;
+        }
+
+        for(int i=0; i<arr.length; i++) {
+           int num = (int) arr[i] - 97;
+           if(arr2[num] == -1){
+               arr2[num] = i;
+           }
+        }
+
+        for(int i=0; i<arr2.length; i++){
+            System.out.print(arr2[i]+" ");
+        }
 
     }
 }
