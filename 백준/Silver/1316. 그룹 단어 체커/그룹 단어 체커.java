11481a54df12c2eea9938936dd820f37d23c4dc0
임선우(Lim Seon Woo) <gomysw1202@gmail.com>
2024-03-20 20:50:59 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,32 +1,37 @@
-import java.util.*;
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+class Main {
+    public static void main(String[] args) throws IOException {
 
-        int n = sc.nextInt(); // 단어의 개수
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
         int cnt = 0;
+        for(int i=0; i<n; i++) {
+            char[] arr = br.readLine().toCharArray();
+            int[] arr2 = new int[26];
+            boolean isGroup = true;
 
-        for (int i = 0; i < n; i++) { // 단어의 개수 만큼 반복
-            String str = sc.next(); // 단어 입력 받기
-            boolean check[] = new boolean[26]; // 알파벳 소문자 수 만큼 배열 생성
-            boolean tmp = true; // 그룹단어 여부 확인용
-
-            for (int j = 0; j < str.length(); j++) {
-                int index = str.charAt(j)-'a'; // 소문자 배열 위치 구하기
-                if(check[index]) { // 만약 이전에 같은 문자가 존재 했다라면
-                    if(str.charAt(j) != str.charAt(j-1)) { // 앞글자가 같은 문자가 아니라면 
-                        tmp = false; // 그룹 단어가 아님
+            for(int j=0; j<arr.length; j++){
+                int a = arr[j]-97;
+                if(arr2[a] > 0){
+                    if(arr[j] != arr[j-1]){
+                        isGroup = false;
                         break;
                     }
-                }else { // 이전에 같은 문자가 존재 하지 않았더라면
-                    check[index] = true; // 문자가 존재했다는 걸 표시하기
+                }else {
+                    arr2[a]++;
                 }
             }
-            if(tmp) { // 그룹 문자라면
+            if(isGroup){
                 cnt++;
             }
         }
-        System.out.println(cnt);
+
+        System.out.print(cnt);
     }
 }
+
+
