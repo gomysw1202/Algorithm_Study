00973f9a6b926f7207fd230bb8ea4322f9cddb64
임선우(Lim Seon Woo) <gomysw1202@gmail.com>
2024-03-24 20:56:40 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,35 +1,36 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         int[][] arr = new int[n][m];
-        int[][] arr2 = new int[n][m];
 
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                arr[i][j] = sc.nextInt();
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                arr2[i][j] = sc.nextInt();
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++){
+                arr[i][j] += Integer.parseInt(st.nextToken());
+                sb.append(arr[i][j] + " ");
             }
+            sb.append("\n");
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                System.out.print(arr[i][j] + arr2[i][j] +" ");
-            }
-            System.out.println("");
-        }
+        System.out.print(sb);
+
     }
 }
